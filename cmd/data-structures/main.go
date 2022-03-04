e5f407c28ee63e8f0a678d8220b146b968c77acd
Felipe Rodrigues <rodrigues882007@gmail.com>
2022-03-04 19:35:11 -0300

    update

--- cmd/data-structures/main.go
+++ cmd/data-structures/main.go
@@ -1,15 +1,26 @@
 package main
 
-import (
-	"data-structures/pkg/dynamicprogramming"
-	"fmt"
-)
+import "data-structures/pkg/trees"
 
 func main() {
-	profits := []int{1, 6, 10, 16}
-	weights := []int{1, 2, 3, 5}
+	//profits := []int{1, 6, 10, 16}
+	//weights := []int{1, 2, 3, 5}
+	//
+	//fmt.Println(dynamicprogramming.Knapsack(profits, weights, 7))
+	//fmt.Println(dynamicprogramming.Knapsack(profits, weights, 6))
 
-	fmt.Println(dynamicprogramming.Knapsack(profits, weights, 7))
-	fmt.Println(dynamicprogramming.Knapsack(profits, weights, 6))
+	tree := trees.AVLTree{}
+
+	tree.Insert(10)
+	tree.Insert(4)
+	tree.Insert(20)
+	tree.Insert(90)
+	tree.Insert(3)
+	tree.Insert(50)
+	tree.Insert(2)
+	tree.Insert(40)
+	tree.Insert(91)
+	tree.Insert(1)
+
 
 }
